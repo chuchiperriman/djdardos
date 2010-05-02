8037fd7b71280bb93a9d737b4d09eccf6088fb7f
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2010-05-02 13:07:29 +0200

    Añadidos empates a los detalles de quipo.

--- dardos/equipos/estadisticas.py
+++ dardos/equipos/estadisticas.py
@@ -33,6 +33,9 @@ class EstadisticasEquipo:
         return Partido.objects.filter(Q(jugado=True) & Q(equipo_local=self.equipo)).exclude(
             Q(ganador=self.equipo) | Q(ganador=None)).count()
 
+    def partidos_empatados_local(self):
+        return Partido.objects.filter(Q(jugado=True) & Q(equipo_local=self.equipo) & Q(ganador=None)).count()
+        
     def partidos_jugados_visitante(self):
         return Partido.objects.filter(Q(jugado=True) & Q(equipo_visitante=self.equipo)).count()
         
@@ -43,6 +46,9 @@ class EstadisticasEquipo:
         return Partido.objects.filter(Q(jugado=True) & Q(equipo_visitante=self.equipo)).exclude(
             Q(ganador=self.equipo) | Q(ganador=None)).count()
     
+    def partidos_empatados_visitante(self):
+        return Partido.objects.filter(Q(jugado=True) & Q(equipo_visitante=self.equipo) & Q(ganador=None)).count()
+            
     def puntos(self):
         return self.partidos_ganados() * 2 + self.partidos_empatados()
         
