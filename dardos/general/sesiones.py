2aa05f85fd78b1122b19aa95fedbf525ae68e474
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2010-11-16 01:10:22 +0100

    Reorganizado acceso a datos de sesión. Título de la web mejor

--- dardos/general/sesiones.py
+++ dardos/general/sesiones.py
@@ -1,4 +1,14 @@
 # -*- mode: python; tab-width: 4; indent-tabs-mode: nil -*-
 
+from djdardos.dardos.models import *
+
 DIVISION_ACTUAL = "division_actual"
 LIGA_ACTUAL = "liga_actual"
+
+def get_liga_actual(request):
+    liga_actual = None
+    if LIGA_ACTUAL in request.session:
+        liga_actual = Liga.objects.get(pk=request.session[LIGA_ACTUAL])
+    elif DIVISION_ACTUAL in request.session:
+        liga_actual = Division.objects.get(pk=request.session[DIVISION_ACTUAL]).get_liga_actual()
+    return liga_actual
--- dardos/templatetags/vista.py
+++ dardos/templatetags/vista.py
@@ -9,16 +9,7 @@ register = template.Library()
 
 @register.inclusion_tag('dardos/vista/display_divliga_actual.html', takes_context = True)
 def display_divliga_actual(context):
-    liga_actual = None
     request = context['request']
-    if LIGA_ACTUAL in request.session:
-        #TODO Igual mejor guardar el nombre para no ir a bbdd
-        liga_actual = Liga.objects.get(pk=request.session[LIGA_ACTUAL])
-    elif DIVISION_ACTUAL in request.session:
-        #TODO Igual mejor guardar el nombre para no ir a bbdd
-        division_actual = Division.objects.get(pk=request.session[DIVISION_ACTUAL])
-        liga_actual = division_actual.get_liga_actual()
-        
-    return {'liga_actual': liga_actual,
+    return {'liga_actual': get_liga_actual(request),
         'current_path': request.path}
 
