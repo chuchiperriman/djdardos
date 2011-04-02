9fa61ba86ce5c7007f00c0b3b8a080aaf2506c18
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2011-04-02 15:03:58 +0200

    Paginación en todos los tags

--- mb/templatetags/mb.py
+++ mb/templatetags/mb.py
@@ -10,29 +10,7 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('mb/tags/lista_tweets.html', takes_context = True)
-def tweets_object(context, objeto):
-    request = context['request']
-    if objeto:
-        ct = ContentType.objects.get_for_model(objeto)
-        tweets = Tweet.objects.filter(sender_type=ct.id, sender_id=objeto.id)
-    else:
-        tweets = []
-    return {'tweets': tweets}
-
-@register.inclusion_tag('mb/tags/lista_tweets.html', takes_context = True)
-def tweets_public(context):
-    """
-    Muestra tweets en el timeline público 
-    """
-    request = context['request']
-
-    print 'aaaaaa',request.GET["page"]
-    
-    ct = ContentType.objects.get_for_model(Equipo)
-    #tweets = Tweet.objects.filter(sender_type=ct.id)
-    tweet_list = Tweet.objects.all()
-
+def get_tweet_pagination (request, tweet_list):
     #TODO cambiar 5 por algo configurable o más real
     paginator = Paginator(tweet_list, 5)
     try:
@@ -45,5 +23,29 @@ def tweets_public(context):
     except (EmptyPage, InvalidPage):
         tweets = paginator.page(paginator.num_pages)
     
-    return {'tweets': tweets}
+    return tweets
+
+@register.inclusion_tag('mb/tags/lista_tweets.html', takes_context = True)
+def tweets_object(context, objeto):
+    request = context['request']
+    if objeto:
+        ct = ContentType.objects.get_for_model(objeto)
+        tweet_list = Tweet.objects.filter(sender_type=ct.id, sender_id=objeto.id)
+    else:
+        tweets_list = []
+        
+    return {'tweets': get_tweet_pagination(request, tweet_list)}
+
+@register.inclusion_tag('mb/tags/lista_tweets.html', takes_context = True)
+def tweets_public(context):
+    """
+    Muestra tweets en el timeline público 
+    """
+    request = context['request']
+
+    ct = ContentType.objects.get_for_model(Equipo)
+    #tweets = Tweet.objects.filter(sender_type=ct.id)
+    tweet_list = Tweet.objects.all()
+
+    return {'tweets': get_tweet_pagination(request, tweet_list)}
 
